9918b792851417544d2004c4fb7b007be2f140d5
cdaringe <cdaringe@gmail.com>
2020-12-12 20:52:36 -0800

    feat: day12, p2

--- day12/p1.ml
+++ day12/p1.ml
@@ -15,7 +15,7 @@ type cmd =
   | L of int
   | R of int
   | F of int
-  [@@deriving show]
+[@@deriving show]
 
 let parse_cmd s =
   Caml.Scanf.sscanf s "%c%d" (fun ch num ->
@@ -30,7 +30,6 @@ let parse_cmd s =
       | _ -> failwith [%string "invalid char %{ch#Char}"])
 
 module Boat = struct
-
   let get_next_angle angle da =
     let v = Int.rem (angle + da) 360 in
     if v > 0 then v else v + 360
@@ -39,35 +38,37 @@ module Boat = struct
     let cmd_s = show_cmd cmd in
     Caml.print_string [%string "%{cmd_s}\n"];
     let { pos = x, y; angle } = boat in
-    let boat' = match cmd with
-    | N num -> { boat with pos = (x, y + num) }
-    | S num -> { boat with pos = (x, y - num) }
-    | E num -> { boat with pos = (x + num, y) }
-    | W num -> { boat with pos = (x - num, y) }
-    | L num ->
-      let next_angle = get_next_angle angle num in
-      Caml.print_string [%string "angle last %{angle#Int} next %{next_angle#Int}\n"];
-      { boat with angle = next_angle }
-    | R num ->
-      let next_angle = get_next_angle angle (-num) in
-      Caml.print_string [%string "angle last %{angle#Int} next %{next_angle#Int}\n"];
-      { boat with angle = next_angle }
-    | F num -> (
-        match angle with
-        | 0 | 360 -> move boat (E num)
-        | 90 -> move boat (N num)
-        | 180 -> move boat (W num)
-        | 270 -> move boat (S num)
-        | _ -> failwith [%string "unhandled angle %{show_angle angle}"] )
+    let boat' =
+      match cmd with
+      | N num -> { boat with pos = (x, y + num) }
+      | S num -> { boat with pos = (x, y - num) }
+      | E num -> { boat with pos = (x + num, y) }
+      | W num -> { boat with pos = (x - num, y) }
+      | L num ->
+          let next_angle = get_next_angle angle num in
+          Caml.print_string
+            [%string "angle last %{angle#Int} next %{next_angle#Int}\n"];
+          { boat with angle = next_angle }
+      | R num ->
+          let next_angle = get_next_angle angle (-num) in
+          Caml.print_string
+            [%string "angle last %{angle#Int} next %{next_angle#Int}\n"];
+          { boat with angle = next_angle }
+      | F num -> (
+          match angle with
+          | 0 | 360 -> move boat (E num)
+          | 90 -> move boat (N num)
+          | 180 -> move boat (W num)
+          | 270 -> move boat (S num)
+          | _ -> failwith [%string "unhandled angle %{show_angle angle}"] )
     in
     Caml.print_string [%string "next_boat: %{show_boaty boat'}\n"];
     boat'
 
-  let distance (ax,ay) (bx,by) =
-    Int.((abs (bx-ax)) + (abs (by-ay)))
+  let distance (ax, ay) (bx, by) = Int.(abs (bx - ax) + abs (by - ay))
 end
 
 let solve () =
   let open Boat in
   get_lines parse_cmd |> CCList.fold_left move { pos = (0, 0); angle = 0 }
-  |> fun boat -> distance (0,0) boat.pos
+  |> fun boat -> distance (0, 0) boat.pos
--- day12/p2.ml
+++ day12/p2.ml
@@ -1,6 +1,61 @@
-(* open Base *)
+open Base
 open Fixture
 
+type pos = int * int [@@deriving show]
+
+type angle = int [@@deriving show]
+
+type boaty = { pos : pos; waypoint : pos } [@@deriving show]
+
+type cmd =
+  | N of int
+  | S of int
+  | E of int
+  | W of int
+  | L of int
+  | R of int
+  | F of int
+[@@deriving show]
+
+let parse_cmd s =
+  Caml.Scanf.sscanf s "%c%d" (fun ch num ->
+      match ch with
+      | 'N' -> N num
+      | 'S' -> S num
+      | 'E' -> E num
+      | 'W' -> W num
+      | 'L' -> L num
+      | 'R' -> R num
+      | 'F' -> F num
+      | _ -> failwith [%string "invalid char %{ch#Char}"])
+
+module Boat = struct
+  let get_next_angle angle da =
+    let v = Int.rem (angle + da) 360 in
+    if v > 0 then v else v + 360
+
+  let to_positive_angle angle = get_next_angle angle 0
+
+  let rec move boat cmd =
+    let { pos = x, y; waypoint = wx, wy } = boat in
+    match cmd with
+    | N num -> { boat with waypoint = (wx, wy + num) }
+    | S num -> { boat with waypoint = (wx, wy - num) }
+    | E num -> { boat with waypoint = (wx + num, wy) }
+    | W num -> { boat with waypoint = (wx - num, wy) }
+    | L angle -> (
+        to_positive_angle angle |> function
+        | 0 -> boat
+        | 90 -> { boat with waypoint = (-wy, wx) }
+        | deg -> move (move boat (L (deg - 90))) (L 90) )
+    | R num -> move boat (L (-num))
+    | F num -> { boat with pos = (x + (num * wx), y + (num * wy)) }
+
+  let distance (ax, ay) (bx, by) = Int.(abs (bx - ax) + abs (by - ay))
+end
+
 let solve () =
-  (* let open CCList in *)
-  get_newline_delimited_lines ()
+  let open Boat in
+  get_lines parse_cmd
+  |> CCList.fold_left move { waypoint = (10, 1); pos = (0, 0) }
+  |> fun boat -> distance (0, 0) boat.pos
